fa7bca6a242a402d22c4ca2416f6a493b5675f7d
Christian Flach <cmfcmf.flach@gmail.com>
2020-05-09 14:31:51 +0200

    Use Postgres

--- app/config/config_heroku.php
+++ app/config/config_heroku.php
@@ -1,12 +1,14 @@
 <?php
 
-$url = parse_url(getenv("JAWSDB_URL"));
+$url = parse_url(getenv("DATABASE_URL"));
 $server = $url["host"];
+$port = $url["port"];
 $username = $url["user"];
 $password = $url["pass"];
 $db = ltrim($url['path'],'/');
 
 $container->setParameter('database_host', $server);
+$container->setParameter('database_port', $port);
 $container->setParameter('database_user', $username);
 $container->setParameter('database_password', $password);
 $container->setParameter('database_name', $db);
