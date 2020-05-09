a35b9852e6f56beeafb55a69712471796b1ff3f7
Christian Flach <cmfcmf.flach@gmail.com>
2020-05-09 13:29:09 +0200

    Run app in PROD on Heroku
    Heroku does not support installing dev dependencies.
    https://github.com/heroku/heroku-buildpack-php/issues/290

--- web/app_heroku.php
+++ web/app_heroku.php
@@ -1,14 +1,7 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Debug\Debug;
 
-// If you don't want to setup permissions the proper way, just uncomment the following PHP line
-// read http://symfony.com/doc/current/setup.html#checking-symfony-application-configuration-and-setup
-// for more information
-//umask(0000);
-
-// This check prevents access to debug front controllers that are deployed by accident to production servers.
 if (!getenv('HEROKU')) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
@@ -16,12 +9,16 @@ if (!getenv('HEROKU')) {
 
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
-Debug::enable();
+include_once __DIR__.'/../var/bootstrap.php.cache';
 
-$kernel = new AppKernel('dev', true);
+$kernel = new AppKernel('prod', false);
+//$kernel = new AppCache($kernel);
+
+// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
+//Request::enableHttpMethodParameterOverride();
+$request = Request::createFromGlobals();
 
 // https://devcenter.heroku.com/articles/deploying-symfony3#trusting-the-heroku-router
-$request = Request::createFromGlobals();
 Request::setTrustedProxies(
     // trust *all* requests
     ['127.0.0.1', $request->server->get('REMOTE_ADDR')],
