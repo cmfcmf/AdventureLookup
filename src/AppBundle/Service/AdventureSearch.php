95351c74cfb12eb166058ea285a485dc04c71d79
Christian Flach <cmfcmf.flach@gmail.com>
2020-06-08 10:45:44 +0200

    Update to PHP7.4, ElasticSearch 7.6.2, Ubuntu 18.04 (#353)
    This PR updates most of our infrastructure to more recent versions.
    
    PHP 7.0 -> 7.4
    ElasticSearch 5.5.3 -> 7.6.2
    Ubuntu 16.04LTS -> 18.04 LTS
    It also:
    
    removes the type parameter from ElasticSearch, which no longer exists
    updates a lot of dependencies to newer minor versions for compatibility with PHP7.4
    decreases unnecessarily high memory used by ElasticSearch from 2G to 1G
    fixes numerours deprecations
    properly uses booleans in DQL instead of numbers, to fix compatibility with PostgreSQL (which will be used for pull request previews on Heroku in an upcoming PR)
    introduces a patch needed for the EasyAdmin bundle to work with PHP 7.4. We cannot upgrade to the next major version of the bundle until we upgrade Symfony to the next major version
    If you want to try this out, you'll have to vagrant destroy your dev environment and re-create it. Gitpod should work fine, if you use the review button below.

--- src/AppBundle/Service/AdventureSearch.php
+++ src/AppBundle/Service/AdventureSearch.php
@@ -29,11 +29,6 @@ class AdventureSearch
     private $indexName;
 
     /**
-     * @var string
-     */
-    private $typeName;
-
-    /**
      * @var TimeProvider
      */
     private $timeProvider;
@@ -43,7 +38,6 @@ class AdventureSearch
         $this->fieldProvider = $fieldProvider;
         $this->client = $elasticSearch->getClient();
         $this->indexName = $elasticSearch->getIndexName();
-        $this->typeName = $elasticSearch->getTypeName();
         $this->timeProvider = $timeProvider;
     }
 
@@ -155,7 +149,6 @@ class AdventureSearch
 
         $result = $this->client->search([
             'index' => $this->indexName,
-            'type' => $this->typeName,
             'body' => [
                 'query' => $query,
                 'from' => self::ADVENTURES_PER_PAGE * ($page - 1),
@@ -199,7 +192,7 @@ class AdventureSearch
                 $hit['_score']
             );
         }, $result['hits']['hits']);
-        $totalResults = $result['hits']['total'];
+        $totalResults = $result['hits']['total']['value'];
         $hasMoreResults = $totalResults > $page * self::ADVENTURES_PER_PAGE;
 
         return [$adventureDocuments, $totalResults, $hasMoreResults, $result['aggregations']];
@@ -213,7 +206,6 @@ class AdventureSearch
 
         $result = $this->client->search([
             'index' => $this->indexName,
-            'type' => $this->typeName,
             'body' => [
                 'query' => [
                     'match' => [
@@ -252,7 +244,6 @@ class AdventureSearch
         $fieldName = $field->getName();
         $response = $this->client->search([
             'index' => $this->indexName,
-            'type' => $this->typeName,
             'body' => [
                 'query' => [
                     'match_phrase_prefix' => [
@@ -309,7 +300,6 @@ class AdventureSearch
 
         $response = $this->client->search([
             'index' => $this->indexName,
-            'type' => $this->typeName,
             'body' => [
                 'size' => 0,
                 'aggregations' => $aggregations,
@@ -507,12 +497,10 @@ class AdventureSearch
                             ],
                         ],
                     ];
-                } else {
-                    $fieldNameForSearch = $fieldName;
-                    if ('string' == $field->getType()) {
-                        $fieldNameForSearch .= '.keyword';
-                    }
-                    $filterMatches[] = ['term' => [$fieldNameForSearch => $value]];
+                } elseif ('string' === $field->getType()) {
+                    $filterMatches[] = ['term' => [$fieldName.'.keyword' => $value]];
+                } elseif ('boolean' === $field->getType()) {
+                    $filterMatches[] = ['term' => [$fieldName => '1' === $value]];
                 }
             }
 
