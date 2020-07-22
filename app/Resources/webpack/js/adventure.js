ae6268352da531901452f81270387e26fe5d00f8
Christian Flach <cmfcmf.flach@gmail.com>
2020-07-22 13:39:20 +0200

    Much progress

--- app/Resources/webpack/js/adventure.js
+++ app/Resources/webpack/js/adventure.js
@@ -1,27 +1,5 @@
-// Returns a function, that, as long as it continues to be invoked, will not
-// be triggered. The function will be called after it stops being called for
-// N milliseconds. If `immediate` is passed, trigger the function on the
-
-const { render } = require("./adventure_autocomplete");
-
-// leading edge, instead of the trailing.
-function debounce(func, wait, immediate) {
-  let timeout;
-  return function () {
-    const context = this,
-      args = arguments;
-    const later = function () {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    const callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) {
-      func.apply(context, args);
-    }
-  };
-}
+import { debounce } from "./util";
+import { render } from "./adventure_autocomplete";
 
 (function () {
   const DEBOUNCE = 250;
--- app/Resources/webpack/js/adventure_autocomplete.jsx
+++ app/Resources/webpack/js/adventure_autocomplete.jsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom";
 import React from "react";
 import AsyncCreatableSelect from "react-select/async-creatable";
 import AsyncSelect from "react-select/async";
+import { debounce } from "./util";
 
 export function render(
   root,
@@ -11,97 +12,102 @@ export function render(
 ) {
   ReactDOM.render(
     <Autocomplete
-      autocompleteInputs={autocompleteInputs}
-      autocompleteSelects={autocompleteSelects}
+      elements={[...autocompleteInputs, ...autocompleteSelects]}
       searchUrl={searchUrl}
     />,
     root
   );
 }
 
-function Autocomplete({ autocompleteInputs, autocompleteSelects, searchUrl }) {
-  return (
-    <>
-      {autocompleteInputs.map((input) => {
-        input.style.display = "none";
-        const fieldName = input.id.split("_").pop();
-        const container = input.parentNode.insertBefore(
-          document.createElement("div"),
-          input
-        );
-        return ReactDOM.createPortal(
-          <AutocompleteInput fieldName={fieldName} searchUrl={searchUrl} />,
-          container
-        );
-      })}
-      {autocompleteSelects.map((select) => {
-        select.style.display = "none";
-        const fieldName = select.id.split("_").pop();
-        const allowMultiple = select.multiple;
-        const allowCreate = !!select.dataset.allowAdd;
-        const container = select.parentNode.insertBefore(
-          document.createElement("div"),
-          select
-        );
-        return ReactDOM.createPortal(
-          <AutocompleteSelect
-            fieldName={fieldName}
-            searchUrl={searchUrl}
-            allowMultiple={allowMultiple}
-            allowCreate={allowCreate}
-          />,
-          container
-        );
-      })}
-    </>
-  );
-}
+function Autocomplete({ elements, searchUrl }) {
+  return elements.map((element) => {
+    const fieldName = element.id.split("_").pop();
+    const allowMultiple = element.tagName === "SELECT" && element.multiple;
+    const allowCreate =
+      element.tagName === "INPUT" || !!element.dataset.allowAdd;
 
-function AutocompleteInput({ searchUrl, fieldName }) {
-  const load = (q, cb) => {
-    const url =
-      searchUrl.replace(/__FIELD__/g, fieldName) +
-      `?q=${encodeURIComponent(q)}`;
-    fetch(url).then((result) =>
-      result
-        .json()
-        .then((result) =>
-          cb(result.map((each) => ({ value: each, label: each })))
-        )
+    element.style.display = "none";
+    const container = element.parentNode.insertBefore(
+      document.createElement("div"),
+      element
     );
-  };
 
-  return (
-    <AsyncCreatableSelect
-      cacheOptions={true}
-      loadOptions={load}
-      defaultOptions={true}
-      allowCreateWhileLoading={true}
-      isClearable={true}
-      isSearchable={true}
-      isMulti={false}
-    />
-  );
+    const defaultValue = (() => {
+      if (element.tagName === "SELECT") {
+        return Array.from(element.options)
+          .filter((option) => option.selected)
+          .map((option) => ({
+            value: option.text,
+            label: option.text,
+          }));
+      } else {
+        return {
+          value: element.value,
+          label: element.value,
+        };
+      }
+    })();
+
+    return ReactDOM.createPortal(
+      <AutocompleteSelect
+        fieldName={fieldName}
+        searchUrl={searchUrl}
+        allowMultiple={allowMultiple}
+        allowCreate={allowCreate}
+        defaultValue={defaultValue}
+        element={element}
+      />,
+      container,
+      fieldName
+    );
+  });
 }
 
-function AutocompleteSelect({
+const AutocompleteSelect = React.memo(function ({
   searchUrl,
   fieldName,
   allowMultiple,
   allowCreate,
+  defaultValue,
+  element,
 }) {
-  const load = (q, cb) => {
-    const url =
-      searchUrl.replace(/__FIELD__/g, fieldName) +
-      `?q=${encodeURIComponent(q)}`;
-    fetch(url).then((result) =>
-      result
-        .json()
-        .then((result) =>
-          cb(result.map((each) => ({ value: each, label: each })))
-        )
-    );
-  };
+  const [value, setValue] = React.useState(defaultValue);
+
+  const onChange = React.useCallback((options) => {
+    setValue(options);
+    if (element.tagName === "SELECT") {
+      for (const option of element.options) {
+        if (
+          Array.isArray(options) &&
+          options.find(({ value }) => value === option.text)
+        ) {
+          option.selected = true;
+        } else if (options !== null && options.value === option.text) {
+          option.selected = true;
+        } else {
+          option.selected = false;
+        }
+      }
+    } else {
+      element.value = options.value;
+    }
+  });
+
+  const load = React.useCallback(
+    debounce((q, cb) => {
+      const url =
+        searchUrl.replace(/__FIELD__/g, fieldName) +
+        `?q=${encodeURIComponent(q)}`;
+      fetch(url).then((result) =>
+        result
+          .json()
+          .then((result) =>
+            cb(result.map((each) => ({ value: each, label: each })))
+          )
+      );
+    }, 250),
+    [fieldName]
+  );
 
   if (allowCreate) {
     return (
@@ -113,6 +119,10 @@ function AutocompleteSelect({
         isClearable={true}
         isSearchable={true}
         isMulti={allowMultiple}
+        closeMenuOnSelect={!allowMultiple}
+        blurInputOnSelect={!allowMultiple}
+        value={value}
+        onChange={onChange}
       />
     );
   } else {
@@ -124,7 +134,11 @@ function AutocompleteSelect({
         isClearable={true}
         isSearchable={true}
         isMulti={allowMultiple}
+        closeMenuOnSelect={!allowMultiple}
+        blurInputOnSelect={!allowMultiple}
+        value={value}
+        onChange={onChange}
       />
     );
   }
-}
+});
